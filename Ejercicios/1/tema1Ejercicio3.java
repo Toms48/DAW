05249db2d2d0738a1a067c8a621ab61660441e84
Tomás Mateo Núñez Ríos <tomasmateo98@gmail.com>
2018-09-21 17:26:35 +0200

    Tema 1 - Ejercicio 3

---
+++ Ejercicios/1/tema1Ejercicio3.java
@@ -0,0 +1,27 @@
+/*
+ * Nombre: tema1Ejercicio3
+ * 
+ * Comentario: Muestra por pantalla 10 palabras en inglés y sus correspondientes traducciones
+ * 
+ * Análisis:
+ * 		-Entrada: No tiene
+ * 		-Salida: Imprime por pantalla 10 Strings
+ */
+
+
+public class tema1Ejercicio3 {
+	
+	public static void main (String[] args) {
+		System.out.println("Flag\t==> Bandera");
+		System.out.println("Book\t==> Libro");
+		System.out.println("Coin\t==> Moneda");
+		System.out.println("Mouse\t==> Raton");
+		System.out.println("Money\t==> Dinero");
+		System.out.println("Ball\t==> Pelota");
+		System.out.println("Glass\t==> Cristal");
+		System.out.println("Pencil\t==> Lapiz");
+		System.out.println("Train\t==> Tren");
+		System.out.println("Car\t==> Coche");
+	}
+}
+
