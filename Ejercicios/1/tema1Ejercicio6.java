8f80501ef98be1719d01be9874a012cdbf31a102
Tomás Mateo Núñez Ríos <tomasmateo98@gmail.com>
2018-09-21 17:27:47 +0200

    Tema 1 - Ejercicio 6

---
+++ Ejercicios/1/tema1Ejercicio6.java
@@ -0,0 +1,22 @@
+/*
+ * Nombre: tema1Ejercicio6
+ * 
+ * Comentario: Mostrará una piramide de base 9* por pantalla
+ * 
+ * Análisis:
+ * 		-Entrada: No tiene
+ * 		-Salida: Imprime por pantalla 5 Strings
+ */
+
+
+public class tema1Ejercicio6 {
+	
+	public static void main (String[] args) {
+		System.out.println("     *     ");
+		System.out.println("    ***    ");
+		System.out.println("   *****   ");
+		System.out.println("  *******  ");
+		System.out.println(" ********* ");
+	}
+}
+
