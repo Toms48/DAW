1f9af6e52110f165cd506b84b572760352c0e7e5
Tomás Mateo Núñez Ríos <tomasmateo98@gmail.com>
2018-09-21 17:27:13 +0200

    Tema 1 - Ejercicio 4

---
+++ Ejercicios/1/tema1Ejercicio4.java
@@ -0,0 +1,44 @@
+/*
+ * Nombre: tema1Ejercicio4
+ * 
+ * Comentario: Muestra por pantalla el horario
+ * 
+ * Análisis:
+ * 		-Entrada: No tiene
+ * 		-Salida: Imprime por pantalla muchos Strings 
+ */
+
+
+public class tema1Ejercicio4 {
+	
+	public static void main (String[] args) {
+		
+		System.out.println(" ");
+		System.out.println("\t o-------o-----------o--------------o---------------o----------------o---------------o");
+		System.out.println("\t | HORA  |   LUNES   |    MARTES    |   MIERCOLES   |     JUEVES     |    VIERNES    |");
+		System.out.println("\t o-------o-----------o--------------o---------------o----------------o---------------o");
+		System.out.println("\t | 08:00 | Sistemas  | Programacion | Programacion  |       FOL      | Programacion  |");
+		System.out.println("\t | 09:00 |           |              |               |                |               |");
+		System.out.println("\t o-------o-----------o--------------o---------------o----------------o---------------o");
+		System.out.println("\t | 09:00 | Sistemas  | Programacion | Programacion  | Sistemas       | Programacion  |");
+		System.out.println("\t | 10:00 |           |              |               |                |               |");
+		System.out.println("\t o-------o-----------o--------------o---------------o----------------o---------------o");
+		System.out.println("\t | 10:00 | Lenguaje  | Programacion | Programacion  | Sistemas       | Entorno       |");
+		System.out.println("\t | 11:00 | de marcas |              |               |                |               |");
+		System.out.println("\t o-------o-----------o--------------o---------------o----------------o---------------o");
+		System.out.println("\t | 11:00 |           |              |               |                |               |");
+		System.out.println("\t | 11:30 |           |              |               |                |               |");
+		System.out.println("\t o-------o-----------o--------------o---------------o----------------o---------------o");
+		System.out.println("\t | 11:30 | Lenguaje  | Sistemas     | Base de Datos | Base de Datos  | Entorno       |");
+		System.out.println("\t | 12:30 | de marcas |              |               |                |               |");
+		System.out.println("\t o-------o-----------o--------------o---------------o----------------o---------------o");
+		System.out.println("\t | 12:30 | Entorno   | Sistemas     | Base de Datos | Base de Datos  | Lenguaje de   |");
+		System.out.println("\t | 13:30 |           |              |               |                | marcas        |");
+		System.out.println("\t o-------o-----------o--------------o---------------o----------------o---------------o");
+		System.out.println("\t | 13:30 |    FOL    |     FOL      | Base de Datos | Base de Datos  | Lenguaje de   |");
+		System.out.println("\t | 14:30 |           |              |               |                | marcas        |");
+		System.out.println("\t o-------o-----------o--------------o---------------o----------------o---------------o");
+		System.out.println(" ");
+	}
+}
+
