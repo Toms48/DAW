ef5a25d545795bb9dd41554ce1a496cfa83a02f4
Tomás Mateo Núñez Ríos <tomasmateo98@gmail.com>
2018-09-21 17:23:44 +0200

    Hola Mundo!

---
+++ Ejercicios/1/holaMundo.java
@@ -0,0 +1,15 @@
+/*
+ * Nombre: holaMundo
+ * 
+ * Comentario: Muestra por pantalla el famoso "Hola Mundo!"
+ * 
+ */
+
+
+public class holaMundo {
+	
+	public static void main (String[] args) {
+		System.out.println("Hola Mundo!");
+	}
+}
+
