cd494bbb3bb5d6442910391ad2baf6b47d94f24d
Tomás Mateo Núñez Ríos <tomasmateo98@gmail.com>
2018-09-21 17:26:20 +0200

    Tema 1 - Ejercicio 2

---
+++ Ejercicios/1/tema1Ejercicio2.java
@@ -0,0 +1,20 @@
+/*
+ * Nombre: tema1Ejercicio2
+ * 
+ * Comentario: Muestra por pantalla mi nombre, dirección y número de teléfono
+ * 
+ * Análisis:
+ * 		-Entrada: No tiene
+ * 		-Salida: Imprime por pantalla tres Strings
+ */
+
+
+public class tema1Ejercicio2 {
+	
+	public static void main (String[] args) {
+		System.out.println("Tom\u00e1s Mateo N\u00fa\u00f1ez R\u00edos");
+		System.out.println("Calle Almeria, Numero 35");
+		System.out.println("628119707");
+	}
+}
+
