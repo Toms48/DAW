ee2811a03f52d58879ef81b7e7fee2cffb35cabe
Tomás Mateo Núñez Ríos <tomasmateo98@gmail.com>
2018-09-21 17:28:25 +0200

    Tema 1 - Ejercicio 8

---
+++ Ejercicios/1/tema1Ejercicio8.java
@@ -0,0 +1,32 @@
+/*
+ * Nombre: tema1Ejercicio8
+ * 
+ * Comentario: Las pirameredes anteriores pero con el vértice invertido
+ * 
+ * Análisis:
+ * 		-Entrada: No tiene
+ * 		-Salida: Imprime por pantalla 13 Strings
+ */
+
+
+public class tema1Ejercicio8 {
+	
+	public static void main (String[] args) {
+		System.out.println(" ********* ");
+		System.out.println("  *******  ");
+		System.out.println("   *****   ");
+		System.out.println("    ***    ");
+		System.out.println("     *     ");
+		
+		System.out.println("");
+		
+		System.out.println(" ************* ");
+		System.out.println("  *         *  ");
+		System.out.println("   *       *   ");
+		System.out.println("    *     *    ");
+		System.out.println("     *   *     ");
+		System.out.println("      * *      ");
+		System.out.println("       *       ");	
+	}
+}
+
