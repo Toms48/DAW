03c28a0d86f9794527cd030e58db8ba5c61fc2ff
Tomás Mateo Núñez Ríos <tomasmateo98@gmail.com>
2018-09-21 17:25:51 +0200

    Tema 1 - Ejercicio 1

---
+++ Ejercicios/1/tema1Ejercicio1.java
@@ -0,0 +1,18 @@
+/*
+ * Nombre: tema1Ejercicio1
+ * 
+ * Comentario: Mostrará mi nombre por pantalla
+ * 
+ * Análisis:
+ * 		-Entrada: No tiene
+ * 		-Salida: Imprime por pantalla un String
+ */
+
+
+public class tema1Ejercicio1 {
+	
+	public static void main (String[] args) {
+		System.out.println("Tom\u00e1s Mateo N\u00fa\u00f1ez R\u00edos");
+	}
+}
+
