b99e16da26fbb93d8138e07a41d2215f2c92637d
Tomás Mateo Núñez Ríos <tomasmateo98@gmail.com>
2018-09-21 18:01:27 +0200

    Tema 1 - Ejercicio 9

---
+++ Ejercicios/1/tema1Ejercicio9.java
@@ -0,0 +1,57 @@
+/*
+ * Nombre: tema1Ejercicio9
+ * 
+ * Comentario: Mostrará un dibujo por pantalla
+ */
+
+
+public class tema1Ejercicio9 {
+	
+	public static void main (String[] args) {
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("              .,-:;//;:=,");
+		System.out.println("          . :H@@@MM@M#H/.,+%;,");
+		System.out.println("       ,/X+ +M@@M@MM%=,-%HMMM@X/,");
+		System.out.println("     -+@MM; $M@@MH+-,;XMMMM@MMMM@+-");
+		System.out.println("    ;@M@@M- XM@X;. -+XXXXXHHH@M@M#@/.");
+		System.out.println("  ,%MM@@MH ,@%=             .---=-=:=,.");
+		System.out.println("  =@#@@@MX.,                -%HX$$%%%:;");
+		System.out.println(" =-./@M@M$                   .;@MMMM@MM:");
+		System.out.println(" X@/ -$MM/                    . +MM@@@M$");
+		System.out.println(",@M@H: :@:                    . =X#@@@@-");
+		System.out.println(",@@@MMX, .                    /H- ;@M@M=");
+		System.out.println(".H@@@@M@+,                    %MM+..%#$.");
+		System.out.println(" /MMMM@MMH/.                  XM@MH; =;");
+		System.out.println("  /%+%$XHH@$=              , .H@@@@MX,");
+		System.out.println("   .=--------.           -%H.,@@@@@MX,");
+		System.out.println("   .%MM@@@HHHXX$$$%+- .:$MMX =M@@MM%.");
+		System.out.println("     =XMMM@MM@MM#H;,-+HMM@M+ /MMMX=");
+		System.out.println("       =%@M@M#@$-.=$@MM@@@M; %M%=");
+		System.out.println("         ,:+$+-,/H#MMMMMMM@= =,");
+		System.out.println("               =++%%%%+/:-.");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("         __                              ___   __        .ama     ,");
+		System.out.println("      ,d888a                          ,d88888888888ba.  ,88´I)   d");
+		System.out.println("     a88']8i                         a88'.8'8)   `'8888:88  ' _a8'");
+		System.out.println("   .d8P' PP                        .d8P'.8  d)      '8:88:baad8P'");
+		System.out.println("  ,d8P' ,ama,   .aa,  .ama.g ,mmm  d8P' 8  .8'        88):888P'");
+		System.out.println(" ,d88' d8[ '8..a8'88 ,8I'88( I88' d88   ]IaI'        d8[   ");
+		System.out.println(" a88' dP 'bm8mP8'(8'.8I  8[      d88'    `'         .88 ");
+		System.out.println(",88I ]8'  .d'.8     88' ,8' I[  ,88P ,ama    ,ama,  d8[  .ama.g");
+		System.out.println("[88' I8, .d' ]8,  ,88B ,d8 aI   (88',88'8)  d8[ '8. 88 ,8I'88[");
+		System.out.println("]88  `888P'  `8888' '88P'8m'    I88 88[ 8[ dP 'bm8m88[.8I  8[");
+		System.out.println("]88,          _,,aaaaaa,_       I88 8'  8 ]P'  .d' 88 88' ,8' I[");
+		System.out.println("`888a,.  ,aadd88888888888bma.   )88,  ,]I I8, .d' )88a8B ,d8 aI");
+		System.out.println("  '888888PP''        `8''''''8   '888PP'  `888P'  `88P'88P'8m'");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+	}
+}
+
