c2a7a5ebd83273e22f3e71aa8e239b07c29a0b5d
Tomás Mateo Núñez Ríos <tomasmateo98@gmail.com>
2018-09-21 17:28:02 +0200

    Tema 1 - Ejercicio 7

---
+++ Ejercicios/1/tema1Ejercicio7.java
@@ -0,0 +1,24 @@
+/*
+ * Nombre: tema1Ejercicio7
+ * 
+ * Comentario: Mostrará el contorno de una piramide de base 9* por pantalla
+ * 
+ * Análisis:
+ * 		-Entrada: No tiene
+ * 		-Salida: Imprime por pantalla 7 Strings
+ */
+
+
+public class tema1Ejercicio7 {
+	
+	public static void main (String[] args) {
+		System.out.println("       *       ");
+		System.out.println("      * *      ");
+		System.out.println("     *   *     ");
+		System.out.println("    *     *    ");
+		System.out.println("   *       *   ");
+		System.out.println("  *         *  ");
+		System.out.println(" ************* ");
+	}
+}
+
