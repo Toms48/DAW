f2235e023fda7d72bcb6bd0b818b91b95e138dfb
Tom_s <tomasmateo98@gmail.com>
2018-10-09 18:45:25 +0200

    Programación - Ejercicios 6 y 7
    Solo análisis

---
+++ Programacion/Ejercicios/4/tema4Ejercicio6.java
@@ -0,0 +1,35 @@
+/*
+ * Nombre: tema4Ejercicio6
+ * 
+ * Comentario:  Realiza un programa que calcule el tiempo que tardará en caer un objeto desde una altura h
+ * 				Aplica la fórmula t =√(2h/g)
+ * 				Siendo g = 9.81
+ * 
+ * Análisis: 
+ * 		Entradas: int para la altura
+ * 		Salidas: por pantalla
+ * 
+ */
+
+//PG
+/*
+	Inicio
+		Preguntar y leer altura (h)
+		Calcular el tiempo (t)
+		Mostrar tiempo
+	Fin
+*/
+
+public class tema4Ejercicio6 {
+	
+	public static void main (String[] args) {
+		
+		//Inicio
+			//Preguntar y leer altura (h)
+			//Calcular el tiempo (t)
+			//Mostrar tiempo
+		//Fin
+		
+	}
+}
+
---
+++ Programacion/Ejercicios/4/tema4Ejercicio7.java
@@ -0,0 +1,31 @@
+/*
+ * Nombre: tema4Ejercicio7
+ * 
+ * Comentario: Calcular la media de tres notas
+ * 
+ * Análisis:
+ * 		Entradas: Tres double para las notas
+ * 		Salidas: Por pantalla
+ * 
+ */
+
+//PG
+/*
+	Inicio
+		Preguntar y leer tres notas
+		Calcular y mostrar media
+	Fin
+*/
+
+public class tema4Ejercicio7 {
+	
+	public static void main (String[] args) {
+		
+		//Inicio
+			//Preguntar y leer tres notas
+			//Calcular y mostrar media
+		//Fin
+		
+	}
+}
+
