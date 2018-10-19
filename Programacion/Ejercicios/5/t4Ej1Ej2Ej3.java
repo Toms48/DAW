d6435a9168a85560e0219cf3974795e1b7b14761
Tomás Núñez <tomasmateo98@gmail.com>
2018-10-19 10:32:53 +0200

    Programación - Tema 5 Ejs 1-6

---
+++ Programacion/Ejercicios/5/t4Ej1Ej2Ej3.java
@@ -0,0 +1,52 @@
+/*
+ * Nombre: t4Ej1Ej2Ej3
+ * 
+ * Comentario: Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for, while y do-while.
+ * 
+ * Análisis
+ * 
+ * 		Entradas: No tiene
+ * 
+ * 		Salidas: Por pantalla
+ * 
+ */
+
+public class t4Ej1Ej2Ej3 {
+	
+	public static void main (String[] args) {
+		
+		for(int i=0; i<101; i++){
+			if((i%10 == 0) || (i%10 == 5)){
+				System.out.println(i);
+			}
+		}
+		
+		/*
+		int numero = 0;
+		
+		while(numero<101){
+			
+			if((numero%10 == 0) || (numero%10 == 5)){
+				System.out.println(numero);
+			}
+			
+			numero++;
+		}
+		*/
+		
+		/*
+		int numero = 0;
+		
+		do{
+			if((numero%10 == 0) || (numero%10 == 5)){
+				System.out.println(numero);
+			}
+			
+			numero++;
+		}
+		while(numero<101);
+		*/
+		
+	}
+}
+
---
+++ Programacion/Ejercicios/5/t4Ej4Ej5Ej6.java
@@ -0,0 +1,45 @@
+/*
+ * Nombre: t4Ej4Ej5Ej6
+ * 
+ * Comentario: Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando un bucle for, while, do-while.
+ * 
+ * Análisis
+ * 
+ * 		Entradas: No tiene
+ * 
+ * 		Salidas: Por pantalla
+ * 
+ */
+
+public class t4Ej4Ej5Ej6 {
+	
+	public static void main (String[] args) {
+		
+		
+		for(int i=320; i>=160; i = i-20){
+			System.out.println(i);
+		}
+		
+		
+		/*
+		int numero = 320;
+		
+		while(numero>=160){
+			System.out.println(numero);
+			numero = numero-20;
+		}
+		*/
+		
+		/*
+		int numero = 320;
+		
+		do{
+			System.out.println(numero);
+			numero = numero-20;
+		}
+		while(numero>=160);
+		*/
+		
+	}
+}
+
