f48e6c8b8e1a5334af1dd1637fd94d54327ab5f4
Tomás Núñez <tomasmateo98@gmail.com>
2018-12-19 00:25:34 +0100

    .

--- Programacion/Ejercicios/Util/src/salida/salida.java
+++ Programacion/Ejercicios/Util/src/salida/salida.java
@@ -16,25 +16,50 @@ package salida;
 
 public class salida {
 
-	public void printRPG(String cadena){
+	public static void printRPG(String cadena) throws InterruptedException{
 
 		char[] stringToCharArray = cadena.toCharArray();
-		
-		for (char i : stringToCharArray) {
-			System.out.print(i);
+
+		for (int i=0; i<=(stringToCharArray.length)-1; i++) {
+			//if(i!=' '){
+			if(i==','){
+				Thread.sleep(900);
+			}
+			else{
+				Thread.sleep(204);
+			}
+			//}
+			System.out.print(stringToCharArray[i]);
 		}
 	}
-	
-	public void printlnRPG(String cadena){
-		
+
+	public static void printlnRPG(String cadena) throws InterruptedException{
+
 		char[] stringToCharArray = cadena.toCharArray();
-		
+
 		for (char i : stringToCharArray) {
 			System.out.print(i);
+			if(i!=' '){
+				if(i==','){
+					Thread.sleep(900);
+				}
+				else{
+					Thread.sleep(204);
+				}
+			}
 		}
-		
+
 		System.out.println("");
-		
+
+	}
+
+	public static void main(String[] args) throws InterruptedException{
+
+		String texto = "Hello! Sorry to keep you waiting! \n\n Nah, es coña. \n\nS\nE\nN\nD\n\nN\nU\nD\nE\nS";
+
+		printRPG(texto);
+		//System.out.println("¯\\_(ツ)_/¯");
+
 	}
 	
 }
