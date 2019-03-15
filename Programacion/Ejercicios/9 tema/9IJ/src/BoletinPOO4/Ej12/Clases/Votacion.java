0535053bfd3593a7db6cf37dbc2132e7da260bc1
Tomás Núñez <tomasmateo98@gmail.com>
2019-03-15 11:00:19 +0100

    Programación - Tema 9 Boletín 4 Ejercicio 12
    Empezando.
    Clase candidato acabada.
    Clase Votacion empezada.

---
+++ Programacion/Ejercicios/9 tema/9IJ/src/BoletinPOO4/Ej12/Clases/Votacion.java
@@ -0,0 +1,43 @@
+/* Nombre: Votacion
+ *
+ * Básicas:
+ * 		- numeroVotosTotales => int => Consultable y Modificable
+ *		- arrayCandidatos => ArrayList => Consultable y Modificable
+ *
+ * Derivadas: No tiene
+ *
+ * Compartidas: No tiene
+ *
+ * Restricciones: No tiene
+ */
+
+package BoletinPOO4.Ej12.Clases;
+
+import java.util.ArrayList;
+
+public class Votacion{
+	
+	//Atributos
+	private static int numeroVotosTotales;
+	private ArrayList<Candidato> arrayCandidatos = new ArrayList<Candidato>();
+	
+	//Constructor por defecto
+	
+	//Constructor con parámetros
+	
+	//Gets
+	
+	//Sets
+	
+	//Métodos sobrescritos
+	@Override
+	public String toString () {
+		
+		String s = "";
+		
+		return s;
+	}
+	
+	//Métos añadidos
+	
+}
